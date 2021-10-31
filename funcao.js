664c387e633c05844a0921e1686cf339e42c30fc
anacarolinesilva <anasilva.caroline@hotmail.com>
2021-10-30 23:07:38 -0300

    commit

---
+++ funcao.js
@@ -0,0 +1,16 @@
+// treinando função com soma 
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(7))
+
+// utilizou esse raciocinio: 7 + 0 = 7
+// como só deu um valor (pro n1), que foi para o primeiro parametro
+// já que o n2 já estava pre definido (0) entao acabou ficando valor 0
+// por ele nao ter recebido outro valor embaixo
+// desse jeito nao retorna algo indefinido
+
+
+
